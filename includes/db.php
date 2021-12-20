0727d965a8ece1d792694b810a6e30bda06e2364
MonicaM007 <72134236+MonicaM007@users.noreply.github.com>
2021-12-20 16:00:33 +0000

    .

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 
 $db_config = [
-    "db_host" => "localhost".
+    "db_host" => "localhost",
     "db.name" => "phptutorial",
     "db.user" => "phptutorial",
     "db_pass" => "lZgsTLEc9vZ7",
