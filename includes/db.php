d4f4b3da8b4be2c20967e1cea2fe630053568afc
MonicaM007 <72134236+MonicaM007@users.noreply.github.com>
2021-12-20 15:52:54 +0000

    .

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_config = [
+    "db_host" => "localhost".
+    "db.name" => "phptutorial",
+    "db.user" => "phptutorial",
+    "db_pass" => "lZgsTLEc9vZ7",
+
+];
+
+echo "DB ARRAY LOADED"
+exit();
+
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+require_once(__DIR__.'/includes/db.php')
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 
